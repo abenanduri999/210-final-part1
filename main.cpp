8603056aab90b3a92abcf0367e607900e60d3cdf
Abe <abenanduri1@gmail.com>
2024-12-09 18:38:39 -0800

    second commit

--- main.cpp
+++ main.cpp
@@ -1,1 +1,29 @@
 #include <iostream>
+#include <string>
+#include <ctime>
+
+
+using namespace std; 
+
+struct Customer
+{
+    string name; 
+    string order; 
+}; 
+
+struct Coffee
+{
+    
+};
+
+int main(){
+
+
+
+
+
+
+
+
+    return 0; 
+}
