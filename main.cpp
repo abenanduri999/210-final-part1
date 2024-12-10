d6e95069de46bf68db6fa70a5a149780e65ecf0c
Abe <abenanduri1@gmail.com>
2024-12-09 18:48:39 -0800

    3rd commit

--- main.cpp
+++ main.cpp
@@ -5,22 +5,26 @@
 
 using namespace std; 
 
-struct Customer
-{
-    string name; 
-    string order; 
-}; 
+//struct Customer
+//{
+ //   string name; 
+  //  string order; 
+//}; 
 
 struct Coffee
 {
-    
+    string name; 
+    string order; 
+    Coffee* next; 
 };
 
 int main(){
 
 
-
-
+    string names[30] = {"Olivia", "Liam", "Emma", "Noah", "Ava", "James", "Sophia", "Benjamin", "Isabella", "Elijah", 
+         "Mia", "Lucas", "Amelia", "Henry", "Harper", "Alexander", "Charlotte", "Mason", "Evelyn", "Daniel", 
+         "Scarlett", "Jack", "Grace", "Samuel", "Ella", "William", "Zoe", "Jack", "Lily", "Michael"};
+     //names generated online
 
 
 
